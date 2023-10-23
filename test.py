788cc40f2e2508a0eb138c11d6d8853de53c2fdf
JirkaKlimes <jiriklimes920@gmail.com>
2023-10-23 14:56:50 +0200

    visualization speedup

--- test.py
+++ test.py
@@ -18,16 +18,16 @@ while True:
         loss = np.linalg.norm(n.get_position() - TARGET)
         n.loss = loss
 
-    ax.clear()
+    groot.add_rated_nodes(nodes)
+
     ax.set_xlim(-1, 1)
     ax.set_ylim(-1, 1)
     ax.plot(TARGET[0], TARGET[1], 'ro')
-    for n in groot.sorted_nodes:
+    for n in nodes:
         pos1 = n.get_position()
         pos2 = n.parrent.get_position()
         ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], 'g-', linewidth=0.2)
 
-    groot.add_rated_nodes(nodes)
     print(groot.sorted_nodes[0].loss)
     plt.pause(0.01)
 
