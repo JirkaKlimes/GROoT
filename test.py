7f16834f6b440a94a2ad32f0959833c70679ea37
JirkaKlimes <jiriklimes920@gmail.com>
2023-10-23 12:29:09 +0200

    simple test

---
+++ test.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.groot import GROoT
+
+TARGET = np.random.uniform(-1, 1, 2)
+
+fig = plt.figure()
+ax = fig.add_subplot()
+
+groot = GROoT(2, 16)
+
+
+while True:
+    nodes = groot.create_nodes()
+
+    for n in nodes:
+        loss = np.linalg.norm(n.get_position() - TARGET)
+        n.loss = loss
+
+    ax.clear()
+    ax.plot(TARGET[0], TARGET[1], 'ro')
+    for n in groot.sorted_nodes:
+        pos1 = n.get_position()
+        pos2 = n.parrent.get_position()
+        ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], 'g-', linewidth=0.2)
+
+    groot.add_rated_nodes(nodes)
+    print(groot.sorted_nodes[0].loss)
+    plt.pause(0.3)
+
+plt.show()
